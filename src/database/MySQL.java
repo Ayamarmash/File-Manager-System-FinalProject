a898c5df0cef75315492a912ec00413defa07fc0
Halbonislam <islamhalboni.fw@gmail.com>
2022-12-24 14:58:14 +0200

    database

--- src/database/MySQL.java
+++ src/database/MySQL.java
@@ -1,21 +1,41 @@
 package database;
 
 import logger.Logger;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MySQL implements Database{
-    private static MySQL MySql = null; // Singleton
+public class MySQL {
+
+    private static MySQL MySql = null;
+
     Logger logger;
+    Logger logs;
     private String LocalDataBaseURL = "jdbc:mysql://localhost:3306/mydb";
     private String user = "root";
     private String password = "1234";
-    Connection connectionDataBase = null;
 
+    Connection connectionDataBase = null;
     public Connection getConnection()
     {
+
+        if(MySql==null)
+        {
+            MySql =  new MySQL();
+
+            try {
+                connectionDataBase = DriverManager.getConnection(LocalDataBaseURL, user, password);
+                logger.logInfo("Create Connection to DataBase");
+
+            }
+            catch (SQLException e) {
+                logger.logError(e.getMessage());
+
+            }
+
+        }
+
         return connectionDataBase;
     }
+
 }
