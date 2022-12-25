5650da5a9a1adb65c34cf69629952e6cb3fe8991
Halbonislam <101211273+Halbonislam@users.noreply.github.com>
2022-12-25 17:47:59 +0200

    commit

--- src/versionControl/DatabaseVersionControl.java
+++ src/versionControl/DatabaseVersionControl.java
@@ -10,5 +10,21 @@ public class DatabaseVersionControl implements VersionControl{
 
     int version;
 
+public void createSnapshot(Connection connection) {
+        // Create a new table for the snapshot
+        try {
+            System.out.println("Enter your version: ");
+            Scanner scanner = new Scanner(System.in);
+            version = scanner.nextInt();
+            Statement stmt = connection.createStatement();
+             stmt.executeUpdate("CREATE TABLE snapshot_" + version + " AS SELECT * FROM files ");
 
+
+            log.logInfo("Snapshot is under progress");
+        }
+        catch (SQLException e) {
+            log.logError(e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
