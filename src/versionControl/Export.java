c08939681cda42dbc3f4eb6439e9c922e8a77a9c
yazeedk <36983323+yazeedk@users.noreply.github.com>
2022-12-25 17:48:38 +0200

    export for version

--- src/versionControl/Export.java
+++ src/versionControl/Export.java
@@ -16,7 +16,29 @@ public class Export implements VersionControl{
     int version;
     private ArrayList<String> tables;
     private String databaseName;
+    
+     public void rollback( Connection connection) {
+        try {
+            System.out.println("Enter your version: ");
+            Scanner scanner = new Scanner(System.in);
+            version = scanner.nextInt();
+            Statement stmt = connection.createStatement();
 
+                stmt.executeUpdate("DROP TABLE files");
+
+        } catch (SQLException e) {
+            log.logError(e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            Statement stmt = connection.createStatement();
+               stmt.executeUpdate("CREATE TABLE files" + " AS SELECT FROM snapshot_" + version);
+
+        } catch (SQLException e) {
+            log.logError(e.getMessage());
+            e.printStackTrace();
+        }
    
     }
 }
