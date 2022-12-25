d2ed5d0338b57eceffcf71aa4092c9e0e2b83497
Halbonislam <101211273+Halbonislam@users.noreply.github.com>
2022-12-25 17:47:21 +0200

    commit

---
+++ src/versionControl/DatabaseVersionControl.java
@@ -0,0 +1,12 @@
+package versioncontrol;
+
+import logger.Logger;
+import java.sql.*;
+import java.util.Scanner;
+
+public class DatabaseVersionControl implements VersionControl{
+
+    Logger log;
+
+    int version;
+
